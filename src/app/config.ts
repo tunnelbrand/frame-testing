3c7064cc663a0640e5c338fb95369696eb7bff5a
Tunnel brand <Tunnelbrand@proton.me>
2024-04-13 01:39:42 +0000

    asdf

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,2 @@
 export const PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_URL}`;
