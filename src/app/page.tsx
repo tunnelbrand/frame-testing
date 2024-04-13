8d2b1d3f7a6d0e5a7b403e754d4ba3e8c0b7326d
Tunnel brand <Tunnelbrand@proton.me>
2024-04-13 00:28:40 +0000

    asdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,48 @@
 import Link from "next/link";
+import Image from 'next/image'
+
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffffcc] to-[#ffffcc] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        <Link
+          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-[#ff9900] hover:bg-white/20"
+          href="https://www.ponchobase.com/"
+          target="_blank"
+        >
+          <Image
+            src="/images/brand.png"
+            width={924}
+            height={291}
+            alt="branding"
+          />
+        </Link>
+        <h1 className="text-5xl font-extrabold tracking-tight text-[#ff9900] sm:text-[5rem]">
+          Frame <span className="text-[#9933cc]">Testing</span>
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        <Link
+          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-[#ff9900] hover:bg-white/20"
+          href="https://create.t3.gg/en/usage/first-steps"
+          target="_blank"
+        >
+          <h3 className="text-2xl font-bold">First Steps →</h3>
+          <div className="text-lg">
+            Vivamus a neque id turpis malesuada auctor nec quis tellus. Integer nec rutrum mauris
+          </div>
+        </Link>
+        <Link
+          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-[#ff9900] hover:bg-white/20"
+          href="https://create.t3.gg/en/introduction"
+          target="_blank"
+        >
+          <h3 className="text-2xl font-bold">Documentation →</h3>
+          <div className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut leo diam, vulputate ac elit eget, mattis vehicula metus.
+          </div>
+        </Link>
       </div>
     </main>
   );
